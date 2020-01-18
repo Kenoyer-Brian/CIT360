9b115ab63ffd5e36b907dc015466de2a8a7d7571
ZEKE <ZEKE@BK-DELL-LATITUDE-E6530>
2020-01-17 22:10:09 -0800

    New Semester Updates

--- W3 Java Collections/src/JavaQueue/JavaQueue.java
+++ W3 Java Collections/src/JavaQueue/JavaQueue.java
@@ -10,7 +10,7 @@ import java.util.*;
 /* @author ZEKE (AKA Brian Kenoyer)
 *Name: Brian Kenoyer
 *Class: CIT-360 
-*Date: October 11, 2019
+*Date: January 17, 2020
 */
 public class JavaQueue {
 
--- W3 Java Collections/src/Java_Tree/Tree.java
+++ W3 Java Collections/src/Java_Tree/Tree.java
@@ -9,7 +9,7 @@ import java.util.TreeSet;
 /* @author ZEKE (AKA Brian Kenoyer)
 *Name: Brian Kenoyer
 *Class: CIT-360 
-*Date: October 11, 2019
+*Date: January 17, 2020
 */
 public class Tree {
 
